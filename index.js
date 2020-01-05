106c0f42a70f6d4bcd1d7e81a91486a0fcb0a2eb
Tanvir Walia <tarywalia@gmail.com>
2020-01-05 19:23:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson";
 
-var greetingSpecialGuest = "Hello," + specialGuest + "!";
+var greetSpecialGuest = "" + specialGuest + "!";
 
 var topic = "space";
 
